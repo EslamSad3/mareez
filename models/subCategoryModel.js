be24c9c41b8e060cd51886ad3648e497b966e35c
EslamSad3 <eslam.sa3d96@gmail.com>
2023-08-19 23:23:38 +0300

    adding eslint

---
+++ models/subCategoryModel.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const subCategorySchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+      required: [true, "SubCategory is Required"],
+      unique: [true, "SubCategory Must Be Unique"],
+      minlength: [2, "To Short SubCategory Name"],
+      maxlength: [20, "To Long SubCategory Name"],
+    },
+    slug: {
+      type: String,
+      lowercase: true,
+    },
+    category: {
+      type: mongoose.Schema.ObjectId,
+      ref: "category",
+      required: [true, "SubCategory must have parent category"],
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("subcategory", subCategorySchema);
