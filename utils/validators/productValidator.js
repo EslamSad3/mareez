e31f92d115618a6d9ad99bd178856c33059f9cf2
EslamSad3 <eslam.sa3d96@gmail.com>
2023-08-21 12:35:19 +0300

    product CRUD FIXING

--- utils/validators/productValidator.js
+++ utils/validators/productValidator.js
@@ -1,83 +1,103 @@
-const { check } = require("express-validator");
-const validatorMiddleWare = require("../../middlewares/validatorMiddleWare");
+const { check } = require('express-validator');
+const validatorMiddleWare = require('../../middlewares/validatorMiddleWare');
+const Category = require('../../models/categoryModel');
+const SubCategory = require('../../models/subCategoryModel');
 
 exports.createProductValidator = [
-  check("title")
+  check('title')
     .isLength({ min: 3 })
-    .withMessage("Too Short Product Name")
+    .withMessage('Too Short Product Name')
     .notEmpty()
-    .withMessage("Product title Required"),
-  check("description")
+    .withMessage('Product title Required'),
+  check('description')
     .notEmpty()
-    .withMessage("Product Description is required")
+    .withMessage('Product Description is required')
     .isLength({ max: 3000 })
-    .withMessage("Product Description is too long"),
-  check("quantity")
+    .withMessage('Product Description is too long'),
+  check('quantity')
     .notEmpty()
-    .withMessage("product quantity is required")
+    .withMessage('product quantity is required')
     .isNumeric()
-    .withMessage("product quantity must be a number"),
-  check("sold")
+    .withMessage('product quantity must be a number'),
+  check('sold')
     .optional()
     .isNumeric()
-    .withMessage("product sold quantity must be a number"),
-  check("price")
+    .withMessage('product sold quantity must be a number'),
+  check('price')
     .notEmpty()
-    .withMessage("product price is required ")
+    .withMessage('product price is required ')
     .isNumeric()
-    .withMessage("product price must be a number")
+    .withMessage('product price must be a number')
     .isLength({ max: 32 }),
-  check("priceAfterDisc")
+  check('priceAfterDisc')
     .optional()
     .isNumeric()
-    .withMessage("price after discount must be a number")
+    .withMessage('price after discount must be a number')
     .toFloat()
     .custom((value, { req }) => {
       if (value >= req.body.price) {
-        throw new Error("price after discount must be less than price");
+        throw new Error('price after discount must be less than price');
       }
       return true;
     }),
-  check("colors")
+  check('colors')
     .optional()
     .isArray()
-    .withMessage("colors must be an array of strings"),
-  check("imageCover").notEmpty().withMessage("image Cover is required"),
-  check("images")
+    .withMessage('colors must be an array of strings'),
+  check('imageCover').notEmpty().withMessage('image Cover is required'),
+  check('images')
     .optional()
     .isArray()
-    .withMessage("colors must be an array of strings"),
-  check("category")
+    .withMessage('colors must be an array of strings'),
+  check('category')
     .notEmpty()
-    .withMessage("products must belong to a category")
+    .withMessage('Product must be belong to a category')
     .isMongoId()
-    .withMessage("invalid category id format"),
-  check("brand").optional().isMongoId().withMessage("invalid brand id format"),
-  check("subcategory")
-    .optional()
-    .isMongoId()
-    .withMessage("invalid subcategory id format"),
-  check("rating")
+    .withMessage('Invalid ID formate')
+    .custom((categoryId) =>
+      Category.findById(categoryId).then((category) => {
+        if (!category) {
+          return Promise.reject(
+            new Error(`No category for this id: ${categoryId}`)
+          );
+        }
+      })
+    ),
+  check('brand').optional().isMongoId().withMessage('invalid brand id format'),
+  check('subcategory')
+  .optional()
+  .isMongoId()
+  .withMessage('Invalid ID formate')
+  .custom((subcategoriesIds) =>
+    SubCategory.find({ _id: { $exists: true, $in: subcategoriesIds } }).then(
+      (result) => {
+        if (result.length < 1 || result.length !== subcategoriesIds.length) {
+          return Promise.reject(new Error(`Invalid subcategories Ids`));
+        }
+      }
+    )
+  ),
+  check('rating')
     .optional()
     .isNumeric()
-    .withMessage("rating must be a number")
+    .withMessage('rating must be a number')
     .isLength({ min: 1, max: 5 })
-    .withMessage("rating must be between 1 and 5"),
-  check("rates").optional().isNumeric().withMessage("rating must be a number"),
+    .withMessage('rating must be between 1 and 5'),
+  check('rates').optional().isNumeric().withMessage('rating must be a number'),
   validatorMiddleWare,
 ];
 
 exports.getProductValidator = [
-  check("id").isMongoId().withMessage("Invalid Product ID Formate"),
+  check('id').isMongoId().withMessage('Invalid Product ID Formate'),
   validatorMiddleWare,
 ];
 
 exports.updateProductValidator = [
-  check("id").isMongoId().withMessage("Invalid Product ID Formate"),
+  check('id').isMongoId().withMessage('Invalid Product ID Formate'),
   validatorMiddleWare,
 ];
 
 exports.deleteProductValidator = [
-  check("id").isMongoId().withMessage("Invalid Product ID Formate"),
+  check('id').isMongoId().withMessage('Invalid Product ID Formate'),
   validatorMiddleWare,
 ];
