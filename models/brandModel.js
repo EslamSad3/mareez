398708e6662bd5fa534e52f24e2d8be3b4c73cc5
EslamSad3 <eslam.sa3d96@gmail.com>
2023-08-21 07:49:05 +0300

    Adding Brand Model

---
+++ models/brandModel.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+const brandSchema = new mongoose.Schema({
+  name: {
+    type : String,
+    required : [true,"brand Required"],
+    unique:[true , "brand must be unique"],
+    minlength : [3,"Too Short Name"],
+    maxlength : [20,"Too Long Name"],
+  },
+  slug :{
+    type:String,
+    lowercase: true
+  },
+  image:String
+
+},{timestamps:true});
+
+// create model
+// module.exports = mongoose.model("brand", brandSchema); 
+const brandModel = mongoose.model("brand", brandSchema);
+module.exports = brandModel;
