742d78899e9fba46ec316d80964ac1c3916e1ea5
EslamSad3 <eslam.sa3d96@gmail.com>
2023-09-24 15:06:18 +0300

    move Routes to /routes/indexjs

---
+++ routes/index.js
@@ -0,0 +1,26 @@
+const categoryRoute = require('./categoryRoute');
+const subCategoryRoute = require('./subCategoryRoute');
+const brandRoute = require('./brandRoute');
+const productRoute = require('./productRoute');
+const reviewRoute = require('./reviewRoute');
+const wishListRoute = require('./wishListRoute');
+const addressesRoute = require('./addressesRoute');
+const couponRoute = require('./couponRoute');
+const userRoute = require('./userRoute');
+const authRoute = require('./authRoute');
+
+const mountRoutes = (app) => {
+  app.use('/api/categories', categoryRoute);
+  app.use('/api/subcategories', subCategoryRoute);
+  app.use('/api/brands', brandRoute);
+  app.use('/api/products', productRoute);
+  app.use('/api/reviews', reviewRoute);
+  app.use('/api/wishlist', wishListRoute);
+  app.use('/api/addresses', addressesRoute);
+  app.use('/api/coupons', couponRoute);
+  app.use('/api/users', userRoute);
+  app.use('/api/auth', authRoute);
+};
+
+
+module.exports = mountRoutes
--- server.js
+++ server.js
@@ -1,70 +1,40 @@
 const path = require('path');
 const express = require('express');
 const dotenv = require('dotenv');
-
 dotenv.config();
 const morgan = require('morgan');
 const ApiError = require('./utils/apiError');
 const globalError = require('./middlewares/errorMiddleware');
 const dbConnection = require('./config/db');
-
-// routes
-const categoryRoute = require('./routes/categoryRoute');
-const subCategoryRoute = require('./routes/subCategoryRoute');
-const brandRoute = require('./routes/brandRoute');
-const productRoute = require('./routes/productRoute');
-const reviewRoute = require('./routes/reviewRoute');
-const wishListRoute = require('./routes/wishListRoute');
-const addressesRoute = require('./routes/addressesRoute');
-const couponRoute = require('./routes/couponRoute');
-const userRoute = require('./routes/userRoute');
-const authRoute = require('./routes/authRoute');
-
+// Routes
+const mountRoutes = require('./routes');
 // DB Connection
 dbConnection();
-
 //Express app
 const app = express();
-
 // MiddleWares
 app.use(express.json());
 app.use(express.static(path.join(__dirname, './uploads')));
 // Mount Routes
-app.use('/api/categories', categoryRoute);
-app.use('/api/subcategories', subCategoryRoute);
-app.use('/api/brands', brandRoute);
-app.use('/api/products', productRoute);
-app.use('/api/reviews', reviewRoute);
-app.use('/api/wishlist', wishListRoute);
-app.use('/api/addresses', addressesRoute);
-app.use('/api/coupons', couponRoute);
-app.use('/api/users', userRoute);
-app.use('/api/auth', authRoute);
-
+mountRoutes(app)
 // Error handling ways
-
 // 1 - Create Err Send it to global err handler
 // app.all('*',(req,res,next)=>{
 //   const err = new Error(`Can't find this route : ${req.originalUrl}`)
 //   next(err.message)
 // })
-
 // 2- Create CUustom and reusable err handling middleware
 app.all('*', (req, res, next) => {
   next(new ApiError(`Can't find this route : ${req.originalUrl}`, 400));
 });
-
 // Express Error Handling Middleware (Global) for express
 app.use(globalError);
-
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
   console.log(`Mode : ${process.env.NODE_ENV}`);
 }
-
 const PORT = process.env.PORT || 8000;
 const server = app.listen(PORT, () => console.log(`running on port : ${PORT}`));
-
 //  Handling Errors (rejections) OutSide Express
 process.on('unhandledRejection', (err) => {
   console.error(`unhandledRejection Errors : ${err.name} | ${err.message}`);
