04d925e19912815823dc6280e54679e0f2578521
EslamSad3 <eslam.sa3d96@gmail.com>
2023-10-11 16:45:10 +0300

    contentSecurityPolicy

--- server.js
+++ server.js
@@ -26,12 +26,21 @@ const app = express();
 app.use(express.json({ limit: '20kb' }));
 
 app.use(
+  helmet({
+    contentSecurityPolicy: {
+      directives: {
+        ...helmet.contentSecurityPolicy.getDefaultDirectives(),
+        "script-src": ["'self'", "'unsafe-inline'", "*"],
+      },
+    },
+  })
+);
+app.use(
   cors({
     origin: '*'
   })
 );
 
-app.use(helmet());
 
 app.use(compression());
 
